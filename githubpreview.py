abb5ef8b3dfa58efbf1c31754eb310b7aed1de24
thegrey129 <tims13412@gmail.com>
2026-04-09 18:17:10 -0600

    Add files via upload
    actually loads sprites

--- githubpreview.py
+++ githubpreview.py
@@ -51,7 +51,7 @@ class SpritePreviewer(QMainWindow):
         top_layout.addWidget(self.fps_slider)
 
         fps_layout = QVBoxLayout()
-        top_layout.addWidget(self.fps_text_label)
+        fps_layout.addWidget(self.fps_text_label)
         fps_layout.addWidget(self.fps_value_label)
 
         self.main_layout.addLayout(top_layout)
@@ -63,76 +63,71 @@ class SpritePreviewer(QMainWindow):
         self.timer = QTimer()
         self.is_running = False
 
+        self.timer.timeout.connect(self.next_frame)
+        self.fps_slider.valueChanged.connect(self.update_fps)
 
-        def load_sprites(self):
-            folder = "sprites"
+        self.load_sprites()
+            
+    def load_sprites(self):
+        folder = "sprites"
 
-            if not os.path.exists(folder):
-                self.image_label.setText("No Sprite Loaded")
-                return
+        if not os.path.exists(folder):
+            self.image_label.setText("No Sprite Loaded")
+            return
 
-            files = os.listdir(folder)
+        files = os.listdir(folder)
 
-            sprite_names = []
-            for file in files:
-                if file.endswith(".png") and file.startswith("sprite_"):
-                    sprite_names.append(file)
+        sprite_names = []
+        for file in files:
+            if file.endswith(".png") and file.startswith("sprite_"):
+                sprite_names.append(file)
 
-            sprite_names.sort()
-            self.sprite_files = [os.path.join(folder, name) for name in sprite_names]
+        sprite_names.sort()
+        self.sprite_files = [os.path.join(folder, name) for name in sprite_names]
 
-            if len(self.sprite_files) > 0:
-                self.show_current_sprite()
-            else:
-                self.image_label.setText("No Sprite Loaded")
+        if len(self.sprite_files) > 0:
+            self.current_index = 0
+            self.show_current_sprite()
+        else:
+            self.image_label.setText("No Sprite Loaded")
 
-            def show_current_sprite(self):
-                if len(self.sprite_files) == 0:
-                    return
+    def show_current_sprite(self):
+        if len(self.sprite_files) == 0:
+            return
 
-                pixmap = QPixmap(self.sprite_files[self.current_index])
+        pixmap = QPixmap(self.sprite_files[self.current_index])
 
-                scaled = pixmap.scaled(
-                    200, 200,
-                    Qt.AspectRatioMode.KeepAspectRatio,
-                    Qt.TransformationMode.FastTransformation
-                )
+        if pixmap.isNull():
+            self.image_label.setText("Image Failed to Load")
+            return
 
-                self.image_label.setPixmap(scaled)
-                def next_frame(self):
-                    if len(self.sprite_files) == 0:
-                        return
-                    def update_fps(self):
-                        fps = self.fps_slider.value()
-                        self.fps_value_label.setText(str(fps))
+        scaled = pixmap.scaled(
+            200, 200,
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.FastTransformation
+        )
 
-                        delay_ms = int(1000/ fps)
+        self.image_label.setPixmap(scaled)
 
-                        if self.is_running:
-                            self.timer.start(delay_ms)
+    def next_frame(self):
+        if len(self.sprite_files) == 0:
+            return
 
-                    self.current_index += 1
+        self.current_index += 1
 
-                    if self.current_index >= len(self.sprite_files):
-                        self.current_index = 0
+        if self.current_index >= len(self.sprite_files):
+            self.current_index = 0
 
-                    self.show_current_sprite()
+        self.show_current_sprite()
 
-            self.timer.timeout.connect(self.next_frame)
+    def update_fps(self):
+        fps = self.fps_slider.value()
+        self.fps_value_label.setText(str(fps))
 
-            self.fps_slider.valueChanged.connect(self.update_fps)
+        delay_ms = int(1000 / fps)
 
-            self.load_sprites()
-
-
-
-
-
-
-
-
-
-
+        if self.is_running:
+            self.timer.start(delay_ms)
 
 
 if __name__ == "__main__":
